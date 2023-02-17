5ad0951d934ecea36c1b864f67654c894e9719c5
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-02-16 20:35:37 -0500

    added some useful functions for windowing and globbing for files

--- Data Preparation/data-prep.py
+++ Data Preparation/data-prep.py
@@ -1,10 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mne
+from pathlib import Path
 
 # chb-mit includes 23 channels sampled at 256hz
 
+# function that gets summary files, reads them, and gets seizure timestamps
+def get_seizure_timestamps():
+    pass
 
+# function that searches the data directory for filenames globbed as *.edf (all our data files for the eeg recordings)
+def get_eegs():
+    eeg_paths = Path.glob(Path('./Data'),'*/*.edf')
+    #seizure_paths = Path.glob(Path('./Data'),'*/*.seizures')
+    path_list = list(eeg_paths) #+ list(seizure_paths)
+    return [str(path) for path in path_list]
+
+# function that takes an eeg recording file and splits it into windows based on a window size and sampling frequency
+# NOTE: window size is in seconds-> number of samples will be taken care of automagically by this function
+def window_recordings(file_path, window_size=12):
+    # reading the eeg file
+    eeg_raw = mne.io.read_raw_edf(file_path)
+
+    # removing the redundant channel
+    eeg_raw = eeg_raw.drop_channels(['T8-P8-1']).rename_channels({'T8-P8-0': 'T8-P8'})
+
+    sampling_frequency = int(eeg_raw.info['sfreq'])
+
+    # getting data in the shape (num_channels,samples) where we sample 'sampling_frequency * seconds'
+    eeg_raw_data = eeg_raw.get_data()
+
+    # windowing based on window_size and sampling frequency
+    total_samples = eeg_raw_data.shape[1]
+    curr_window = 0
+    new_data = []
+    while curr_window != total_samples:
+        window = eeg_raw_data[:,curr_window:curr_window+(sampling_frequency*window_size)]
+        # we want to break if the window is less than our desired window size by splicing
+        if window.shape[1]<sampling_frequency*window_size:
+            break
+        #print(window.shape)
+        new_data.append(window)
+        # moving the window by just 1 second
+        curr_window += sampling_frequency
+
+    # we want to return something like (window,num_channels,samples)
+    return np.array(new_data)
+
+# function that takes an eeg recording, and applies the short term fourier transform with window and overlap parameters
+# we filter out noise frequency and DC frequency specific to the CHB-MIT scalp EEG dataset
+def stft_recordings(eeg_data,window=256,overlap=None):
+    # applying stft to data
+    frequencies = mne.time_frequency.stft(eeg_data,wsize=window,tstep=overlap)
+    # removing DC component (0 Hz), the 57-63Hz and 117-123Hz bands (specific for
+    frequencies = np.delete(frequencies, [0, *[i for i in range(57, 64, 1)], *[i for i in range(117, 124, 1)]], axis=1)
+    return frequencies
+
+'''
+    Below is some basic logic I wrote while testing stuff
+'''
+
+'''
 # read in an edf file
 test = mne.io.read_raw_edf('./Data/chb01/chb01_01.edf')
 print(test.info)
@@ -44,5 +100,6 @@ print(test_fft.shape)
 test_fft = np.delete(test_fft,[0,*[i for i in range(57,64,1)],*[i for i in range(117,124,1)]],axis=1)
 
 print(test_fft.shape)
+'''
 
 
