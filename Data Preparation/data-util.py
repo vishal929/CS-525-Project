cf9e5cdbf3d672b26501736bc398952f21e6b4db
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-02-28 11:38:03 -0500

    start data util logic

---
+++ Data Preparation/data-util.py
@@ -0,0 +1,7 @@
+# this file houses logic associated with data preparation for deep learning
+# i.e loading the dataset, and computing class weights
+
+# this function looks at each patients train and validation data for the processed directory
+# we index each example so that at train time or test time, we can load examples just with their indices
+grab_processed_metadata():
+    pass
