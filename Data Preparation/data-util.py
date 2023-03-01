1f6a7d056a4d9ce2ff3c59ed9e3b747a53b08d68
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-02-28 19:57:42 -0500

    added tf dataset via dataset api for loading our training, val, and test data with patient level cross validation

--- Data Preparation/data-util.py
+++ Data Preparation/data-util.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from pathlib import Path
 import numpy as np
 
@@ -6,6 +7,7 @@ import numpy as np
 
 # this function looks at each patients train and validation data for the processed directory
 # we index each example so that at train time or test time, we can load examples just with their indices
+'''DONT USE THE BELOW FUNCTION -> I Just thought i'd leave it here in case something goes wrong with tf data api in future'''
 def grab_processed_metadata(window_size=1):
     # finding the processed files
     ictal_train = Path.glob(Path('./Processed_Data'),'*/'+str(window_size)+'-ictal_train.npy')
@@ -45,7 +47,50 @@ def grab_processed_metadata(window_size=1):
 
     return train_map,val_map
 
+# loading numpy file data and labels
+# 0 will be for interictal, 1 will be for ictal
+def npy_to_tf(filename):
+    filename_str = filename.numpy()
+    #print(filename_str)
+    data = np.load(filename_str)
+    if tf.strings.regex_full_match(filename,'.*interictal.*'):
+        #labels = np.zeros(shape = (data.shape[0]))
+        labels = tf.zeros(shape = (data.shape[0]),dtype=tf.float32)
+    else:
+        #labels = np.ones(shape = (data.shape[0]),dtype = np.int8)
+        labels = tf.ones(shape=(data.shape[0]), dtype=tf.float32)
+
+    #print(data[0].shape)
+    #print(data)
+    data = tf.convert_to_tensor(data,dtype=tf.float32)
+
+    return data,labels
+
+
+''' USE THIS FUNCTION TO GET A DATASET WE CAN LOAD INTO OUR KERAS MODEL'''
 # tensorflow data pipeline based on our metadata
+# we specify a split (either 'train','val','test')
+# we specify a window size
+# we specify a patient to leave out i.e if we specify 'chb01' then patient 1 is not included in train or val
+# # this patient will be included in the test set
+def tf_dataset(split='train',window_size=1,leave_out='chb01'):
+    if split != 'test':
+        dataset = tf.data.Dataset.list_files('./Processed_Data/*/'+str(window_size)+'-'+'*'+split+'.npy')
+    else:
+        # we want the entire data for patient chb01 in this case for testing
+        dataset = tf.data.Dataset.list_files('./Processed_Data/'+leave_out+'/'+str(window_size)+'*.npy')
+    # converting filename to batched numpy array and batched label tensors
+    dataset = dataset.map(lambda x: tf.py_function(npy_to_tf,inp=[x],Tout=[tf.float32,tf.float32]))
+    # taking the batched data and batched labels and flattening them (preserves order)
+    examples = dataset.flat_map(lambda example,label: tf.data.Dataset.from_tensor_slices(example))
+    labels = dataset.flat_map(lambda example,label: tf.data.Dataset.from_tensor_slices(label))
+    # zipping together flattened examples to form final dataset
+    dataset = tf.data.Dataset.zip((examples,labels))
+    return dataset
 
-
-print(grab_processed_metadata()[0][5000])
+''' EXAMPLE OF HOW TO COUNT IN THE DATASET (we need eager execution due to transformations)'''
+'''
+test = tf_dataset(split='val')
+count = test.reduce(0, lambda x,_: x+1).numpy()
+print(count)
+'''
