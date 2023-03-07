ff27c350f12470236e8e39f039ffe6a10507d158
Katie Serova <katie@serov.us>
2023-03-07 15:06:39 -0500

    drafted train function

--- train.py
+++ train.py
@@ -2,16 +2,21 @@
 from Models import model
 from Data_Preparation import data_util
 
-def train(model, tf_dataset):
-    pass
+def train(model, tf_dataset, val_set):
+    # train_batch_size = ?, steps_per_epoch should be num_samples // train_batch_size
+    # val_batch_size = ?, validation_steps should be num_val_samples // val_batch_size
+    trained_model = model.fit(tf_dataset, epochs=6, verbose=2, validation_data=val_set, steps_per_epoch=None, 
+              validation_steps=None)
+    return trained_model
 
 '''
 Want something like the below (also need to run validation on the data after every epoch)
 Essentially, we train for each epoch and then test on validation data
 We have some "patience" so if the val score does not increase for lets say 5 consecutive epochs, then we stop training
 We save only the best model based on validation so far
+'''
 tf_dataset = data_util.tf_dataset('train')
+val_set = data_util.tf_dataset('val')
 model = model.buildModel()
 
-train(model,tf_dataset)
-'''
+train(model,tf_dataset, val_set)
