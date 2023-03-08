f74dee8737641ce97e1a37f701aab36337879228
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-03-07 20:33:03 -0500

    add class weighting to train function and batch_size as parameter

--- train.py
+++ train.py
@@ -6,19 +6,18 @@ from Data_Preparation import data_util
 Not sure if it would be better to write train and test functions from scratch, 
 but Keras has built-in methods for this
 '''
-def train(model, tf_dataset, val_set):
+def train(model, tf_dataset, val_set, batch_size=1):
+    # grabbing class imbalances so we can set this on the fly for training
+    num_interictal, num_ictal = data_util.get_class_counts(tf_dataset)
+    imbalance = round(num_interictal/num_ictal)
+    # we want ictal segments to be weighted more for how imbalanced they are
+    weight = {0:1, 1:imbalance}
     # train_batch_size = ?, steps_per_epoch should be num_samples // train_batch_size
     # val_batch_size = ?, validation_steps should be num_val_samples // val_batch_size
-    trained_model = model.fit(tf_dataset, epochs=6, verbose=2, validation_data=val_set, steps_per_epoch=None, 
-              validation_steps=None)
+    trained_model = model.fit(tf_dataset, epochs=6, verbose=2, validation_data=val_set, steps_per_epoch=None,
+              validation_steps=None,batch_size=batch_size,class_weight=weight)
     return trained_model
 
-'''
-Want something like the below (also need to run validation on the data after every epoch)
-Essentially, we train for each epoch and then test on validation data
-We have some "patience" so if the val score does not increase for lets say 5 consecutive epochs, then we stop training
-We save only the best model based on validation so far
-'''
 tf_dataset = data_util.tf_dataset('train')
 val_set = data_util.tf_dataset('val')
 model = model.buildModel()
