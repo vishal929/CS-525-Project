61a833821c6c19c7a5e9f82ccc1e59ef04162b28
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-03-09 03:16:22 -0500

    add early stopping with patience

--- train.py
+++ train.py
@@ -8,13 +8,16 @@ Not sure if it would be better to write train and test functions from scratch,
 but Keras has built-in methods for this
 '''
 def train(model, tf_dataset, val_set, batch_size=32):
-
+    # want to early stop if the validation loss does not improve for 3 consecutive epochs
+    early_stop = tf.keras.callbacks.EarlyStopping(
+       patience=3
+    )
     # batching and shuffling
     tf_dataset = tf_dataset.shuffle(buffer_size=2000000).batch(batch_size,num_parallel_calls=tf.data.AUTOTUNE)
     val_set = val_set.batch(batch_size,num_parallel_calls=tf.data.AUTOTUNE)
     # train_batch_size = ?, steps_per_epoch should be num_samples // train_batch_size
     # val_batch_size = ?, validation_steps should be num_val_samples // val_batch_size
-    trained_model = model.fit(tf_dataset, epochs=6, verbose=2, validation_data=val_set)
+    trained_model = model.fit(tf_dataset, epochs=6, verbose=2, validation_data=val_set,callbacks=[early_stop])
     return trained_model
 
 # print gpu availability
