9fd2cf6d4a537f2afcaa007f60a1315276b90673
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-03-08 20:35:45 -0500

    increase batch size and buffer_size for shuffle

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ but Keras has built-in methods for this
 def train(model, tf_dataset, val_set, batch_size=32):
 
     # batching and shuffling
-    tf_dataset = tf_dataset.shuffle(buffer_size=1000000).batch(batch_size,num_parallel_calls=tf.data.AUTOTUNE)
+    tf_dataset = tf_dataset.shuffle(buffer_size=2000000).batch(batch_size,num_parallel_calls=tf.data.AUTOTUNE)
     val_set = val_set.batch(batch_size,num_parallel_calls=tf.data.AUTOTUNE)
     # train_batch_size = ?, steps_per_epoch should be num_samples // train_batch_size
     # val_batch_size = ?, validation_steps should be num_val_samples // val_batch_size
@@ -22,7 +22,7 @@ print(tf.config.get_visible_devices())
 
 # printing task before training
 window_size = 1
-batch_size=32
+batch_size=128
 leave_out = 'chb01'
 print('training, batch_size = ' + str(batch_size) + ', leave_out=' + str(leave_out))
 
