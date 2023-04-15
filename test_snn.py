c0d1d009d8514b9486529cb25f596dc95dcf5d5c
Katie Serova <katie@serov.us>
2023-04-15 00:01:16 -0400

    attempts to convert to snn

--- test_snn.py
+++ test_snn.py
@@ -3,34 +3,35 @@ from tensorflow import keras
 import nengo_dl
 import nengo
 import numpy as np
+from Data_Preparation import data_util
+from Models import model
 from keras_data_format_converter import convert_channels_first_to_last
 
-# around 92k params
-# Building the convolutional neural network:
+#modified buildModel for channel last
 def buildModel():
-    input = keras.Input(shape=(1, 22, 114))  # consists of 22 EEG channels
+    input = keras.Input(shape=(22, 114, 1))  # consists of 22 EEG channels
 
     # We apply the ReLU activation function to each of the three 2D convolutional layers
     # I just swapped relu with leaky relu in order to avoid a "dead" gradient problem
 
     # The first layer has 16 kernals of size (22, 5)
-    x = keras.layers.Conv2D(16, (22, 5), strides=(1, 2), padding='valid', data_format="channels_first",
+    x = keras.layers.Conv2D(16, (22, 5), strides=(1, 2), padding='valid', data_format="channels_last",
                             activation=tf.nn.leaky_relu)(input)
     # For each channel, MaxPool2D() takes the max value within a designated window of the input of size (1, 2)
-    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_first", padding='valid')(x)
+    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_last", padding='valid')(x)
     # Then, we normalize the max value just collected
     x = keras.layers.BatchNormalization()(x)
 
     # The second layer has 32 kernals of size (1, 3)
-    x = keras.layers.Conv2D(32, (1, 3), strides=(1, 1), padding='valid', data_format="channels_first",
+    x = keras.layers.Conv2D(32, (1, 3), strides=(1, 1), padding='valid', data_format="channels_last",
                             activation=tf.nn.leaky_relu)(x)
-    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_first", )(x)
+    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_last", )(x)
     x = keras.layers.BatchNormalization()(x)
 
     # The third and last convolutional layer has 64 kernals also of size (1, 3)
-    x = keras.layers.Conv2D(64, (1, 3), strides=(1, 1), padding='valid', data_format="channels_first",
+    x = keras.layers.Conv2D(64, (1, 3), strides=(1, 1), padding='valid', data_format="channels_last",
                             activation=tf.nn.leaky_relu)(x)
-    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_first", )(x)
+    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_last", )(x)
     x = keras.layers.BatchNormalization()(x)
 
     # To create the 2 fully connected layers, we first flatten the extracted features
@@ -68,57 +69,35 @@ def buildModel():
     print('CNN model successfully built')
     return model
 
-# helper function to explicitly strip a model of its dropout layers for nengo conversion
-def remove_dropout_layers(model):
-    layers = model.layers
-    input_layer = layers[0]
-    x = input_layer.output
-    for l in layers[1:]:
-        if not isinstance(l,keras.layers.Dropout):
-            x = l(x)
-    new_model = keras.Model(input_layer.input,x)
-    print(new_model.summary())
-    return new_model
+new_model = buildModel()
+old_model = keras.models.load_model('Trained Models/chb01----1----seizure_number:1')
+#old_model = convert_channels_first_to_last(old_model)
+# print(old_model.summary())
+# weights = [layer.get_weights() for layer in old_model.layers]
+# for layer, weight in zip(old_model.layers, weights):
+#     print(layer)
+#     for i in weight:
+#         print(i.shape)
+# print(old_model.get_config())
+# for layer, weight in zip(new_model.layers, weights):
+#     for i in weight: print(i.shape)
+#     print(layer)
+#     for i in layer.get_weights(): print(i.shape)
+#     layer.set_weights(weight)
+# print(new_model.summary())
 
-# loading a model from a saved checkpoint
-# model path should be something like '...../Trained Models/chb01-1.ckpt'
-# chb01 is the leave-out and 1 is the window_size
-def load_model(model_path):
-    model = buildModel()
-    model.load_weights(model_path)
 
-# function that converts this keras model into an snn in nengo
-def convert_snn(saved_weights_directory=None):
-    # model = buildModel()
-    model = keras.models.load_model('../Trained Models/chb01----1----seizure_number:1')
-    # model = convert_channels_first_to_last(model)
-    # # loading weights if they exist
-    # if saved_weights_directory:
-    #     model.load_weights(saved_weights_directory)
-    # need to remove dropout layers because they are not supported in nengo
-    stripped_model = remove_dropout_layers(model)
-    swap_activations = {tf.nn.leaky_relu:nengo_dl.SpikingLeakyReLU()}
-    converted = nengo_dl.Converter(stripped_model,max_to_avg_pool=True,inference_only=True,allow_fallback=False,
-                                   swap_activations=swap_activations)
-    print(converted.verify())
-    return converted
-
-converted = convert_snn()
+# converted = model.convert_snn()
 
 # test_set = data_util.tf_dataset('test',window_size=1,leave_out='chb01')
 
-# with nengo.Network() as net:
+# with converted.net:
 #     # no need for any training
 #     nengo_dl.configure_settings(
 #         trainable=None,
 #         stateful=True,
 #         keep_history=True,
 #     )
-#     with nengo_dl.Simulator(converted.net) as sim:
-
-#         print(sim.predict(x=np.ones(shape=(2,23,2508))))
-# converted = convert_snn('../Trained Models/chb01----1----seizure_number:1/variables/variables')
-# model = keras.models.load_model('../Trained Models/chb01----1----seizure_number:1')
-# print(model.summary())
-#with nengo_dl.Simulator(converted.net) as sim:
-#    sim.run(3)
+# with nengo_dl.Simulator(converted.net) as sim:
+#     sim.compile()
+#     print(sim.evaluate(x=test_set))
