ac3ba7ab0fbd8b99b3441582d89d956965dd38de
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-04-17 22:24:35 -0400

    got better results, added train and val prepping for debugging

--- test_snn.py
+++ test_snn.py
@@ -93,6 +93,9 @@ def buildModel():
 # globbing for saved model paths of this specific window_size
 window_size=1
 
+use_train = False
+use_val = False
+
 model_paths = Path.glob(Path(ROOT_DIR,'Trained Models'),'chb*----'+str(window_size)+'----seizure_number_*')
 model_paths = list(model_paths)
 
@@ -114,9 +117,12 @@ for model_path in model_paths:
     converted = model.convert_snn(model_path)
 
     # we can throw out train and val, no need for that in evaluation
-    _,_,test_set = data_util.get_seizure_leave_out_data(seizure_number=seizure_number,
+    train_set,validation_set,test_set = data_util.get_seizure_leave_out_data(seizure_number=seizure_number,
                                                         window_size=window_size,
                                                         patient=patient)
+
+    if not use_val: del(validation_set)
+    if not use_train: del(train_set)
 
     # split into examples and labels
     examples = test_set.map(lambda example,label: example)
@@ -147,6 +153,41 @@ for model_path in model_paths:
 
     #print(converted.verify(inputs=np.ones((1,1,22,114))))
 
+    if use_train:
+        # prepping train data
+        train_examples = train_set.map(lambda example, label, imbalance: example)
+        train_labels = train_set.map(lambda example, label, imbalance: label)
+
+        train_examples = list(train_examples.as_numpy_iterator())
+        train_labels = list(train_labels.as_numpy_iterator())
+
+        num_train_examples = len(train_examples)
+        train_examples = np.stack(train_examples, axis=0)
+        train_labels = np.stack(train_labels)
+
+        train_examples = train_examples.reshape(num_train_examples, 1, -1)
+        train_examples = np.tile(train_examples, (1, timesteps, 1))
+        train_labels = np.expand_dims(train_labels, axis=-1).repeat(num_features, axis=-1)
+        train_labels = np.expand_dims(train_labels, axis=1).repeat(timesteps, axis=1)
+
+    if use_val:
+        # prepping val data
+        validation_examples = validation_set.map(lambda example, label: example)
+        validation_labels = validation_set.map(lambda example, label: label)
+
+        validation_examples = list(validation_examples.as_numpy_iterator())
+        validation_labels = list(validation_labels.as_numpy_iterator())
+
+        num_val_examples = len(validation_examples)
+
+        validation_examples = np.stack(validation_examples, axis=0)
+        validation_labels = np.stack(validation_labels)
+
+        validation_examples = validation_examples.reshape(num_val_examples, 1, -1)
+        validation_examples = np.tile(validation_examples, (1, timesteps, 1))
+        validation_labels = np.expand_dims(validation_labels, axis=-1).repeat(num_features, axis=-1)
+        validation_labels = np.expand_dims(validation_labels, axis=1).repeat(timesteps, axis=1)
+
     with converted.net:
         # no need for any training
         nengo_dl.configure_settings(
@@ -167,56 +208,108 @@ for model_path in model_paths:
                             converted.outputs[converted.model.output]:keras.metrics.Recall(name='recall'),
                             converted.outputs[converted.model.output]:keras.metrics.AUC(name='auc'),
                         })
-            pred = sim.predict(x=examples,n_steps=timesteps)
-            #print(sim.predict(x=np.expand_dims(examples[0,:,:],axis=0)))
-            #sim.evaluate(x=examples,y=labels)
+            if use_train:
+                train_pred = sim.predict(x=train_examples,n_steps=timesteps)
+            if use_val:
+                val_pred = sim.predict(x=validation_examples,n_steps=timesteps)
+            test_pred = sim.predict(x=examples,n_steps=timesteps)
 
-    pred = list(pred.values())[0]
+    test_pred = list(test_pred.values())[0]
 
-    pred = np.squeeze(pred)
+    test_pred = np.squeeze(test_pred)
     # decoding by taking the mean activation
-    pred = np.mean(pred,axis=-1)
+    test_pred = np.mean(test_pred,axis=-1)
 
     # if decoded value < 0 , then we give 0, if decoded value > 0, we give 1
-    pred = np.greater_equal(pred,0).astype(np.int32)
+    test_pred = np.greater_equal(test_pred,0).astype(np.int32)
+    labels = np.squeeze(labels[:,0,0])
+
+    if use_train:
+        train_pred = list(train_pred.values())[0]
+
+        train_pred = np.squeeze(train_pred)
+        # decoding by taking the mean activation
+        train_pred = np.mean(train_pred, axis=-1)
+
+        # if decoded value < 0 , then we give 0, if decoded value > 0, we give 1
+        train_pred = np.greater_equal(train_pred, 0).astype(np.int32)
+        train_labels = np.squeeze(train_labels[:, 0, 0])
+
+        # getting false positives,false negatives, true negatives, and true positives
+        train_fp = np.logical_and(np.equal(train_pred, 1), np.equal(train_labels, 0)).astype(np.int32).sum()
+        train_tp = np.logical_and(np.equal(train_pred, 1), np.equal(train_labels, 1)).astype(np.int32).sum()
+        train_fn = np.logical_and(np.equal(train_pred, 0), np.equal(train_labels, 1)).astype(np.int32).sum()
+        train_tn = np.logical_and(np.equal(train_pred, 0), np.equal(train_labels, 0)).astype(np.int32).sum()
+        print('train false positive: ' + str(train_fp))
+        print('train true positive: ' + str(train_tp))
+        print('train false negative: ' + str(train_fn))
+        print('train true negative: ' + str(train_tn))
+
+        # computing accuracy
+        train_accuracy = np.equal(train_pred, train_labels).astype(np.int32).mean()
+        print('train accuracy: ' + str(train_accuracy))
+
+        # computing precision
+        train_precision = train_tp / (train_fp + train_tp)
+        print('train precision: ' + str(train_precision))
+
+        # computing recall
+        train_recall = train_tp / (train_tp + train_fn)
+        print('train recall: ' + str(train_recall))
+
+    if use_val:
+        val_pred = list(val_pred.values())[0]
+
+        val_pred = np.squeeze(val_pred)
+        # decoding by taking the mean activation
+        val_pred = np.mean(val_pred, axis=-1)
+
+        # if decoded value < 0 , then we give 0, if decoded value > 0, we give 1
+        val_pred = np.greater_equal(val_pred, 0).astype(np.int32)
+        val_labels = np.squeeze(validation_labels[:, 0, 0])
+
+        # getting false positives,false negatives, true negatives, and true positives
+        val_fp = np.logical_and(np.equal(val_pred, 1), np.equal(val_labels, 0)).astype(np.int32).sum()
+        val_tp = np.logical_and(np.equal(val_pred, 1), np.equal(val_labels, 1)).astype(np.int32).sum()
+        val_fn = np.logical_and(np.equal(val_pred, 0), np.equal(val_labels, 1)).astype(np.int32).sum()
+        val_tn = np.logical_and(np.equal(val_pred, 0), np.equal(val_labels, 0)).astype(np.int32).sum()
+        print('val false positive: ' + str(val_fp))
+        print('val true positive: ' + str(val_tp))
+        print('val false negative: ' + str(val_fn))
+        print('val true negative: ' + str(val_tn))
+
+        # computing accuracy
+        val_accuracy = np.equal(val_pred, val_labels).astype(np.int32).mean()
+        print('val accuracy: ' + str(val_accuracy))
+
+        # computing precision
+        val_precision = val_tp / (val_fp + val_tp)
+        print('val precision: ' + str(val_precision))
+
+        # computing recall
+        val_recall = val_tp / (val_tp + val_fn)
+        print('val recall: ' + str(val_recall))
+
+    # test accuracy
+    test_acc = np.equal(test_pred, labels).astype(np.int32).mean()
+    print('test accuracy: ' + str(test_acc))
+
+    # storing detection accuracy in dictionary so we can compute mean at the end
+    if patient in patient_accs:
+        (patient_accs[patient]).append(test_acc)
+    else:
+        patient_accs[patient] = [test_acc]
 
     #print(pred.shape)
     #print(pred)
-    labels = np.squeeze(labels[:,0,0])
     #print('pred shape: ' + str(pred.shape))
     #print('labels shape: ' + str(labels.shape))
     #print('accuracy: ' + str((np.equal(np.round(pred),labels)).astype(np.int32).mean()))
 
-    # getting false positives,false negatives, true negatives, and true positives
-    fp = np.logical_and(np.equal(pred,1),np.equal(labels,0)).astype(np.int32).sum()
-    tp = np.logical_and(np.equal(pred,1),np.equal(labels,1)).astype(np.int32).sum()
-    fn = np.logical_and(np.equal(pred,0),np.equal(labels,1)).astype(np.int32).sum()
-    tn = np.logical_and(np.equal(pred,0),np.equal(labels,0)).astype(np.int32).sum()
-    print('false positive: ' + str(fp))
-    print('true positive: ' + str(tp))
-    print('false negative: ' + str(fn))
-    print('true negative: ' + str(tn))
 
-    # computing accuracy
-    accuracy = np.equal(pred,labels).astype(np.int32).mean()
-    print('accuracy: ' + str(accuracy))
-
-    # computing precision
-    precision = tp/(fp+tp)
-    print('precision: ' + str(precision))
-
-    # computing recall
-    recall = tp/(tp+fn)
-    print('recall: ' + str(recall))
-
-    # storing detection accuracy in dictionary so we can compute mean at the end
-    if patient in patient_accs:
-        (patient_accs[patient]).append(accuracy)
-    else:
-        patient_accs[patient] = [accuracy]
 
 # computing patient accuracies for each experiment
 for patient in patient_accs:
     total_acc = np.array(patient_accs[patient]).mean()
-    print('accuracy on all experiments for patient: ' + str(patient) + ' acc: ' + str(total_acc))
+    print('testing accuracy on all experiments for patient: ' + str(patient) + ' acc: ' + str(total_acc))
 
