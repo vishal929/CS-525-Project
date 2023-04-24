468be42f854d2e339c6f2cc73b9eb97e14ad65bf
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-04-24 12:29:54 -0400

    remove channel last model and increase timesteps

--- test_snn.py
+++ test_snn.py
@@ -11,85 +11,6 @@ from pathlib import Path
 import re
 #from keras_data_format_converter import convert_channels_first_to_last
 
-#modified buildModel for channel last
-def buildModel():
-    input = keras.Input(shape=(22, 114, 1))  # consists of 22 EEG channels
-
-    # We apply the ReLU activation function to each of the three 2D convolutional layers
-    # I just swapped relu with leaky relu in order to avoid a "dead" gradient problem
-
-    # The first layer has 16 kernals of size (22, 5)
-    x = keras.layers.Conv2D(16, (22, 5), strides=(1, 2), padding='valid', data_format="channels_last",
-                            activation=tf.nn.leaky_relu)(input)
-    # For each channel, MaxPool2D() takes the max value within a designated window of the input of size (1, 2)
-    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_last", padding='valid')(x)
-    # Then, we normalize the max value just collected
-    x = keras.layers.BatchNormalization()(x)
-
-    # The second layer has 32 kernals of size (1, 3)
-    x = keras.layers.Conv2D(32, (1, 3), strides=(1, 1), padding='valid', data_format="channels_last",
-                            activation=tf.nn.leaky_relu)(x)
-    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_last", )(x)
-    x = keras.layers.BatchNormalization()(x)
-
-    # The third and last convolutional layer has 64 kernals also of size (1, 3)
-    x = keras.layers.Conv2D(64, (1, 3), strides=(1, 1), padding='valid', data_format="channels_last",
-                            activation=tf.nn.leaky_relu)(x)
-    x = keras.layers.MaxPool2D(pool_size=(1, 2), data_format="channels_last", )(x)
-    x = keras.layers.BatchNormalization()(x)
-
-    # To create the 2 fully connected layers, we first flatten the extracted features
-    x = keras.layers.Flatten()(x)
-
-    # Dropout layers are added before both of the 2 fully connected layers at a rate of 0.5
-    x = keras.layers.Dropout(0.5)(x)
-    # First fully connected layer has an output size of 256 and applies the sigmoid activation function
-    # Changed activation to relu just to test things out
-    #x = keras.layers.Dense(units=256, activation=tf.nn.sigmoid)(x)
-    x = keras.layers.Dense(units=256, activation=tf.nn.leaky_relu)(x)
-
-    x = keras.layers.Dropout(0.5)(x)
-    # Second fully connected layerhas an output size of 2 and applies the softmax activation function
-    #output = keras.layers.Dense(units=2, activation=tf.nn.softmax)(x)
-    output = keras.layers.Dense(units=1, activation=tf.nn.sigmoid)(x)
-
-    model = keras.Model(input, output)
-    # Finally, we compute the cross-entropy loss between true labels and predicted labels to account for
-    # the class imbalance between seizure and non-seizure depicting data
-    #loss_func = keras.losses.categorical_crossentropy
-    loss_func = keras.losses.BinaryCrossentropy()
-    optim = keras.optimizers.RMSprop(learning_rate=0.00001)
-    metrics = [
-        keras.metrics.TruePositives(name='tp'),
-        keras.metrics.FalsePositives(name='fp'),
-        keras.metrics.TrueNegatives(name='tn'),
-        keras.metrics.FalseNegatives(name='fn'),
-        keras.metrics.BinaryAccuracy(name='accuracy'),
-        keras.metrics.Precision(name='precision'),
-        keras.metrics.Recall(name='recall'),
-        keras.metrics.AUC(name='auc'),
-    ]
-    model.compile(loss=loss_func, optimizer=optim, metrics=metrics, )
-    print('CNN model successfully built')
-    return model
-
-#new_model = buildModel()
-#old_model = keras.models.load_model('Trained Models/chb01----1----seizure_number:1')
-#old_model = convert_channels_first_to_last(old_model)
-# print(old_model.summary())
-# weights = [layer.get_weights() for layer in old_model.layers]
-# for layer, weight in zip(old_model.layers, weights):
-#     print(layer)
-#     for i in weight:
-#         print(i.shape)
-# print(old_model.get_config())
-# for layer, weight in zip(new_model.layers, weights):
-#     for i in weight: print(i.shape)
-#     print(layer)
-#     for i in layer.get_weights(): print(i.shape)
-#     layer.set_weights(weight)
-# print(new_model.summary())
-
 # globbing for saved model paths of this specific window_size
 window_size=1
 
@@ -113,13 +34,13 @@ for model_path in model_paths:
     seizure_number = int(re.sub("[^0-9]","",basename[-2:]))
     # specify timesteps to repeat input for snn
     if window_size==1:
-        timesteps = 30
+        timesteps = 200
     else:
         timesteps = 23
     # specify synaptic filter
     synapse=0.01
     # specify scaling of firing rates
-    scale_firing_rates=1000
+    scale_firing_rates=250
 
     print('testing snn for patient: ' + str(patient) + ' on seizure number: ' + str(seizure_number))
 
